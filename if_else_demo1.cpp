62afc86c5eafd7d9c511bebcaf6991575eab20ce
Md. Mohiuddin <mohiuddin.ice.ru@gmail.com>
2023-11-08 08:17:11 +0600

    if else demo

---
+++ if_else_demo1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x = 2;
+
+    if (x > 3) {
+        printf("value of x is greater than 3");
+    } else if (x == 3) {
+        printf("x is equal 3");
+    } else {
+        printf("value of x is less than 3");
+    }
+}
